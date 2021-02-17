0e4b1a801c84a37eb0cfcf9514e69aff0f4f08fa
Sergio <sherrera725@csu.fullerton.edu>
2021-02-17 15:47:30 -0800

    fixed file linking+UI changes

--- gallery.php
+++ gallery.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Photo Gallery</title>
+    <title>Photo Album Gallery</title>
 </head>
 <body>
     <h1>Photo Album Uploader Gallery</h1>
@@ -19,10 +19,10 @@
 
 
         echo '<p>Photo uploaded successfully.</p>';
-        echo htmlspecialchars($photoTitle);
-        echo htmlspecialchars($photoDate);
-        echo htmlspecialchars($photographer);
-        echo htmlspecialchars($location);
+        echo htmlspecialchars($photoTitle).'<br/>';
+        echo htmlspecialchars($photoDate).'<br/>';
+        echo htmlspecialchars($photographer).'<br/>';
+        echo htmlspecialchars($location).'<br/>';
     ?>
 </body>
 </html>
