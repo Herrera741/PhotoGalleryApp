e436a44374e2f4ab5702c4b2a8dc8f9f8cd438ec
Sergio <sherrera725@csu.fullerton.edu>
2021-02-17 16:45:43 -0800

    upload script test added to gallery.php

--- php/gallery.php
+++ php/gallery.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Photo Gallery</title>
-</head>
-<body>
-    <h1>Photo Album Uploader Gallery</h1>
-    <h2>Upload Results</h2>
-    <?php
-        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"uploads/".$_FILES["fileToUpload"]["name"]); 
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//$target_file = "myFile.jpg";
 
-
-
-        $target_dir = "uploads/";
-        // create short variable names
-        $photoTitle = $_POST['photoTitle'];
-        $photoDate = $_POST['photoDate'];
-        $photographer = $_POST['photographer'];
-        $location = $_POST['location'];
-
-
-        echo '<p>Photo uploaded successfully.</p>';
-        echo htmlspecialchars($photoTitle);
-        echo htmlspecialchars($photoDate);
-        echo htmlspecialchars($photographer);
-        echo htmlspecialchars($location);
-    ?>
-</body>
-</html>
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]). " has been uploaded.";
+     } else {
+    echo "Sorry, there was an error uploading your file.";
+}
+?>
